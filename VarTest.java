ab8f1bbcf85aca039f4c657037e204b66cd58a6b
Bigmi <emycon@qq.com>
2020-02-14 11:36:15 +0800

    first push

---
+++ VarTest.java
@@ -0,0 +1,10 @@
+public class VarTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Define a variable a is");
+        int a;
+        a = 5;
+        System.out.println(a);
+    }
+}
